59bbe33f2889bbea3d5e47aa8372ce13440ca1cd
B-Nilson <nilson.brayden@gmail.com>
2025-10-11 12:02:35 -0700

    ensure new aqhi_colouyrs changes passes tests

--- R/get_aqhi_colours.R
+++ R/get_aqhi_colours.R
@@ -82,11 +82,15 @@ get_aqhi_colours <- function(values = c(1:10, "+", NA), types = "aqhi") {
   is_pm25 <- types %in% "pm25_1hr"
   if (any(is_pm25)) {
     values[is_pm25] <- values[is_pm25] |>
-      AQHI_plus(detailed = FALSE)
+      AQHI_plus(detailed = FALSE) |> 
+      as.character()
   }
+  values[!is_pm25 & values == "11"] <- "+"
 
   # Convert factors to integer values (1-11, or NA)
-  aqhi_levels <- as.numeric(values)
+  aqhi_levels <- values |>
+    factor(levels = c(1:10, "+")) |>
+    as.numeric()
 
   # Replace missing AQHI levels with 12 (missing)
   aqhi_levels[is.na(aqhi_levels)] <- 12
